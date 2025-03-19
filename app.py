c3de222b83c32605b07de781ad106783c4a06559
Tom28824 <lon24nguy02@mpw.ac.uk>
2025-03-19 16:40:14 +0000

    create table

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+import sqlite3
+con = sqlite3.connect("login.db")
+cur = con.cursor()
+cur.execute(""" CREATE TABLE IF NOT EXISTS user(
+                name VARCHAR(10)NOT NULL PRIMARY KEY,
+                password VARCHAR(20)NOT NULL
+                )""")
+con.commit()
+con.close()
+
 @app.route("/sign-up", methods=["GET", "POST"])
 def signup():
     if request.method == "GET":
